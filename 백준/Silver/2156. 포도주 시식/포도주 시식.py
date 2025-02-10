67b8ef88b1cce626eb141adc6c3671dce6db8f24
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2025-02-10 23:11:48 +0900

    [Silver I] Title: 포도주 시식, Time: 112 ms, Memory: 110916 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,18 +1,21 @@
 N = int(input())
 
-if N == 0:
-    print(0)
+podos = []
+for _ in range(N):
+    podos.append(int(input()))
+
+if N <= 2:
+    print(sum(podos))
     exit()
 
-podos = [0] + [int(input()) for _ in range(N)]  # 1-based index 유지
-dp = [0] * (N + 1)
 
-dp[1] = podos[1]
-if N > 1:
-    dp[2] = podos[1] + podos[2]
+dp = [0] * N
+dp[0] = podos[0]
+dp[1] = podos[0] + podos[1]
 
-if N >= 3:
-    for i in range(3, N+1):
-        dp[i] = max(dp[i-1], dp[i-2] + podos[i], dp[i-3] + podos[i-1] + podos[i])
 
-print(dp[N])
+
+for i in range(2, N):
+    dp[i] = max(dp[i-1], dp[i-2] + podos[i], dp[i-3] + podos[i-1] + podos[i])
+
+print(dp[N-1])
