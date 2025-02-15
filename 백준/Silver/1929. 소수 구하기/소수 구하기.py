a572144b96d49bd0aad5870fabacca70b5681874
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2025-02-15 19:02:12 +0900

    [Silver III] Title: 소수 구하기, Time: 300 ms, Memory: 119472 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+primes = []
+ans = []
+for i in range(2, m+1):
+    isPrime = True
+    for p in primes:
+        if p * p > i: break
+        if i % p == 0:
+            isPrime = False
+            break
+    if isPrime:
+        primes.append(i)
+        if i >= n:
+            ans.append(i)
+
+for p in ans:
+    # if p < n: continue
+    print(p)
