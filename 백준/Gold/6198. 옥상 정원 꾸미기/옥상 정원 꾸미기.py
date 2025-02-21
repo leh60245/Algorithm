d27e8b1909c942e966f548ec58e8ba1e652197df
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2025-02-21 16:12:35 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 136 ms, Memory: 112580 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -1,13 +1,37 @@
-N = int(input())
-stack = []
-answer = 0
+import sys
+from collections import deque
 
-for i in range(1,N+1):
-    height = int(input())
-    while stack and stack[-1] <= height:
-        stack.pop()
-    stack.append(height)
-    answer += len(stack) -1
 
-print(answer)
+def foo(n, arr):
+    stack = deque()
 
+    ans = 0
+    for i in arr:
+        if not stack:
+            stack.append(i)
+            continue
+        if stack[-1] > i:
+            ans += len(stack)
+            stack.append(i)
+            continue
+        while stack:
+            stack.pop()
+            if stack and stack[-1] > i:
+                break
+        ans += len(stack)
+        stack.append(i)
+    return ans
+
+
+if __name__ == "__main__":
+    try:
+        sys.stdin = open("input.txt", "r")
+    except FileNotFoundError:
+        pass
+
+    input = sys.stdin.readline
+
+    n = int(input())
+    arr = [int(input()) for _ in range(n)]
+
+    print(foo(n, arr))
