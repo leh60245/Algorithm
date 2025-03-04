ab35e00de11beb810cbf3c7284848a3838e686f1
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2025-03-04 18:04:25 +0900

    [Silver II] Title: N과 M (10), Time: 92 ms, Memory: 109540 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,37 @@
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r")
+except FileNotFoundError:
+    pass
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+visited = [0 for _ in range(N)]
+result = [0 for _ in range(M)]
+
+
+def backtracking(cnt):
+    if cnt == M:
+        print(*result)
+        return
+    tmp = -1
+    for i in range(N):
+        if visited[i] or arr[i] == tmp:
+            continue
+        if cnt > 0 and arr[i] < result[cnt-1]:
+            continue
+        visited[i] = 1
+        tmp = arr[i]
+        result[cnt] = arr[i]
+        backtracking(cnt + 1)
+        visited[i] = 0
+
+
+def main():
+    backtracking(0)
+
+
+if __name__ == "__main__":
+    main()
