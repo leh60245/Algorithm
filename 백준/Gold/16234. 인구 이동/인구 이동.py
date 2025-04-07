41c89ac1a0cd41f19abf4919237b5af9b10f29fc
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2025-04-07 15:40:52 +0900

    [Gold IV] Title: 인구 이동, Time: 2392 ms, Memory: 209876 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,71 +1,75 @@
 import sys
 from collections import deque
 
+DEBUG = False
+try:
+    sys.stdin = open("example5.txt", "r")
+    DEBUG = True
+    print()
+except FileNotFoundError:
+    pass
 input = sys.stdin.readline
 N, L, R = map(int, input().split())
-arr = []
+grid = []
 for _ in range(N):
-    arr.append(list(map(int, input().split())))
+    grid.append(list(map(int, input().split())))
 
 
-# 국경선 공유하는 두 나라 인구 차이가 L <= ? <= R 라면 국경선을 하루 동안 연다
-#   위 조건으로 국경선이 모두 열렸다면, 인구 이동을 시작한다.
-# 국경선이 열려 있는 각 나라는 연합이다.
-# 연합을 이루는 각 칸의 인구수는 sum_people // num_block 이다.
-# 연합을 해체하고, 모든 국경선을 닫는다.
-# answer: 인구 이동이 며칠 동안 발생하는가?
-
-def in_box(i, j):
-    return 0 <= i < N and 0 <= j < N
-
-
-def bfs(si, sj):
-    # [1] 생성
-    global arr
+def bfs(si, sj, v):
     q = deque()
-    global v
-    save_points = []
-    sum_people, num_blocks = 0, 0
+    people = 0
+    cnt = 0
+    path = list()
 
-    # [2] 초기 설정
     q.append((si, sj))
-    v[si][sj] = 1
-    save_points.append((si, sj))
-    sum_people, num_blocks = arr[si][sj], 1
-
-    # [3]
+    v.add((si, sj))
+    people += grid[si][sj]
+    cnt += 1
+    path.append((si, sj))
     while q:
         ci, cj = q.popleft()
-        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+
+        for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             ni, nj = ci + di, cj + dj
-            if in_box(ni, nj) and not v[ni][nj] and L <= abs(arr[ci][cj] - arr[ni][nj]) <= R:
+            if not (0 <= ni < N and 0 <= nj < N) or (ni, nj) in v:
+                continue
+            if L <= abs(grid[ni][nj] - grid[ci][cj]) <= R:
                 q.append((ni, nj))
-                v[ni][nj] = 1
-                save_points.append((ni, nj))
-                sum_people += arr[ni][nj]
-                num_blocks += 1
+                v.add((ni, nj))
+                people += grid[ni][nj]
+                cnt += 1
+                path.append((ni, nj))
 
-    for i, j in save_points:
-        arr[i][j] = sum_people // num_blocks
+    return people, cnt, path, v
 
-    if num_blocks > 1:
-        return 1
-    else:
-        return 0
+def normalize(p: int, c: int, v: list, g):
+    n = p // c
+    for i, j in v:
+        g[i][j] = n
+    return g
 
 
-ans = 0
-while True:
-    v = [[0] * N for _ in range(N)]
-    is_move = 0
-    for i in range(N):
-        for j in range(N):
-            if not v[i][j]:
-                is_move += bfs(i, j)
+def main():
+    global grid
+    day = 0
+    while True:
+        un = 0
+        visited = set()
+        for i in range(N):
+            for j in range(N):
+                if (i, j) in visited:
+                    continue
+                people, cnt, path, visited = bfs(i, j, visited)
+                grid = normalize(people, cnt, path, grid)
+                un += 1
 
-        # 더 이상 변화가 없다면 끝 -> 인구에 변화가 있냐 없냐는 어떻게 알아낼 수 있을까? 국경선이 열리면 해결?
-    if not is_move:
-        break
-    ans += 1
 
-print(ans)
+        if un == N * N:
+            print(day)
+            return
+        day += 1
+
+
+
+if __name__ == "__main__":
+    main()
