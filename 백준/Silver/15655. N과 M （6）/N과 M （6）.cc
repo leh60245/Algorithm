a5dbf990134f1e2ca868a4285be7a3539c37840e
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2025-04-23 11:13:02 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -3,14 +3,14 @@ using namespace std;
 
 
 int N, M;
+vector<int> num;
 vector<int> arr;
-vector<int> ans;
 // vector<int> vis;
 
 void resize_all()
 {
+    num.resize(N, 0);
     arr.resize(N, 0);
-    ans.resize(N, 0);
     // vis.resize(N, 0)
 }
 
@@ -18,16 +18,16 @@ void permutation(int depth)
 {
     if (depth == M)
     {
-        for (int i = 0 ; i<M ; i++) cout << ans[i] << ' ';
+        for (int i = 0 ; i<M ; i++)
+            cout << num[arr[i]] << ' ';
         cout << '\n';
         return;
     }
-    int base = arr[0];
-    if (depth != 0) base = ans[depth-1];
-    for (int n=0 ; n<N ; n++)
+    int st = 0;
+    if (depth != 0) st = arr[depth-1] + 1;
+    for (int n=st ; n<N ; n++)
     {
-        if (depth >0 && arr[n] <= base) continue;
-        ans[depth] = arr[n];
+        arr[depth] =n;
         permutation(depth+1);
     }
 
@@ -41,8 +41,8 @@ int main()
     cin >> N >> M;
     resize_all();
 
-    for (int n=0 ; n<N ; n++) cin >> arr[n];
-    sort(arr.begin(), arr.end());
+    for (int n=0 ; n<N ; n++) cin >> num[n];
+    sort(num.begin(), num.end());
 
     permutation(0);
 }
