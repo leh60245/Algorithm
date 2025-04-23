d6aa58dfb790f3bd569a264a8b9bfdeb207f7e44
LeeEuiHyoung <55617010+leh60245@users.noreply.github.com>
2025-04-23 11:42:25 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int N, M;
+vector<int> num;
+vector<int> arr;
+vector<int> vis;
+
+void resize_all()
+{
+    num.resize(N, 0);
+    arr.resize(N, 0);
+    vis.resize(N, 0);
+}
+
+void permutation(int cnt)
+{
+    if (cnt == M)
+    {
+        for (int i = 0 ; i<M ; i++)
+            cout << num[arr[i]] << ' ';
+        cout << '\n';
+        return;
+    }
+    int tmp = 0;
+    for (int n=0 ; n<N ; n++)
+    {
+        if (vis[n] || tmp == num[n]) continue;
+        vis[n] = 1;
+        arr[cnt] = n;
+        tmp = num[n];
+        permutation(cnt + 1);
+        vis[n] = 0;
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    resize_all();
+
+    for (int n=0 ; n<N ; n++) cin >> num[n];
+    sort(num.begin(), num.end());
+
+    permutation(0);
+}
