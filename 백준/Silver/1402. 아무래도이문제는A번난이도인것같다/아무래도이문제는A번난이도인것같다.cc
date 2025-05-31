36ac1caa8ac682e1efbb5d8e6bf5aac1be6e45ec
Lee Eui-Hyeong <55617010+leh60245@users.noreply.github.com>
2025-05-31 18:53:29 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vi = vector<int>;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    int a, b;
+    while (T--)
+    {
+        cin >> a >> b;
+        cout << "yes\n";
+    }
+}
