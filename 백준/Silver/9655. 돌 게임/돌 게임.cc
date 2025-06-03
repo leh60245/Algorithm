c946a58431dfee77f543c6960cce4edcbf20e4aa
Lee Eui-Hyeong <55617010+leh60245@users.noreply.github.com>
2025-06-03 16:59:04 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N;
+
+    if (N % 2 == 1)
+        cout << "SK";
+    else cout << "CY";
+}
