424c0817fe48d6728d4dd293d00c79077f98c2de
Lee Eui-Hyeong <55617010+leh60245@users.noreply.github.com>
2025-07-12 19:27:56 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 3356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,54 +1,58 @@
-#include <bits/stdc++.h>
+// main.cpp
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
 using namespace std;
 using vi = vector<int>;
-using vvi = vector<vi>;
+using Edge = tuple<int, int, int>; // (cost, a, b)
 
-int find(int x, vi& arr)
-{
-    if (arr[x] < 0)
-        return x;
-    return arr[x] = find(arr[x], arr);
+int V, E;
+vector<Edge> edges;
+vi parent;
+
+int find(int x) {
+    if (parent[x] < 0) return x;
+    return parent[x] = find(parent[x]);
 }
 
-bool uni(int u, int v, vi& arr)
-{
-    u = find(u, arr);
-    v = find(v, arr);
-    if (u == v)
-        return false;
-    if (arr[v] < arr[u])
-        swap(u, v);
-    if (arr[u] == arr[v])
-        arr[u]--;
-    arr[v] = u;
+
+bool unite(int u, int v) {
+    int pu = find(u), pv = find(v);
+    if (pu == pv) return false; 
+
+    if (parent[pu] == parent[pv]) parent[pu]--;
+    if (parent[pu] < parent[pv])
+        parent[pv] = pu;
+    else
+        parent[pu] = pv;
     return true;
 }
 
-vi arr;
-vector<tuple<int, int, int>> edges;
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int V, E; cin >> V >> E;
-    arr.resize(V+1, -1);
-    for (int e=0; e < E ; e++)
-    {
-        int n1, n2, c; cin >> n1 >> n2 >> c;
-        edges.push_back({c, n1, n2});
-    }
-    sort(edges.begin(), edges.end());
-    int cost = 0;
-    for (int i = 0 ; i < E; i++)
-    {
-        int c, n1, n2;
-        tie(c, n1, n2) = edges[i];
-        if (!uni(n1, n2, arr)) continue;
-        cost += c;
-    }
-    cout << cost;
+    cin >> V >> E;
+    edges.reserve(E);
+    parent.assign(V + 1, -1); 
 
+    for (int i = 0; i < E; ++i) {
+        int a, b, cost;
+        cin >> a >> b >> cost;
+        edges.emplace_back(cost, a, b);
+    }
 
+    sort(edges.begin(), edges.end()); 
+
+    long long totalCost = 0;
+    for (const auto& [cost, a, b] : edges) {
+        if (unite(a, b)) {
+            totalCost += cost;
+        }
+    }
+
+    cout << totalCost << '\n';
+    return 0;
 }
