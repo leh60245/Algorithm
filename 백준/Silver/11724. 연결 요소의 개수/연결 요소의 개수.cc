0579371ade01709b4cbcf5c628e7da032d78646c
Lee Eui-Hyeong <55617010+leh60245@users.noreply.github.com>
2025-12-30 23:59:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 72 ms, Memory: 6536 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,50 +1,42 @@
+// Authored by : BaaaaaaaaaaarkingDog
+// Co-authored by : -
+// http://boj.kr/888234c79cf844af82b67a3c9518b1dd
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M;
-vector<int> graph[1001];
-int vis[1001];
+vector<int> adj[1005];
+bool vis[1005];
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m;
 
-void bfs(int start)
-{
-    deque<int> q;
+  // 그래프 입력 처리
+  cin >> n >> m;  
+  while(m--){
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);    
+  }
 
-    q.push_back(start);
-    vis[start] = 1;
-    while (!q.empty())
-    {
-        int cur = q.front(); q.pop_front();
-        for (int next: graph[cur])
-        {
-            if (vis[next] == 1) continue;
-            q.push_back(next);
-            vis[next] = 1;
-        }
+  // BFS
+  int num = 0;
+  for(int i = 1; i <= n; i++){
+    if(vis[i]) continue;
+    num++;
+    queue<int> q;
+    q.push(i);
+    vis[i] = true;
+    while(!q.empty()){
+      int cur = q.front();
+      q.pop();
+      for(auto nxt : adj[cur]){
+        if(vis[nxt]) continue;
+        q.push(nxt);
+        vis[nxt] = true;
+      }
     }
-}
-
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> N >> M;
-
-    for (int m=0; m<M ; m++)
-    {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-
-    int ans = 0;
-    for (int i=1; i<=N; i++)
-    {
-        if (vis[i] == 0)
-        {
-            bfs(i);
-            ans++;
-        }
-    }
-    cout << ans;
+  }
+  cout << num;
 }
