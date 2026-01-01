d7280f1d7f0277b1d0f035415ff04ad69eac19c5
Lee Eui-Hyeong <55617010+leh60245@users.noreply.github.com>
2026-01-01 23:38:02 +0900

    [Silver IV] Title: 설탕 배달, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -4,24 +4,18 @@ try:
     sys.stdin = open("input.txt", "r")
 except FileNotFoundError:
     pass
-input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
 
-dp = [-1] * (N+1)
-dp[0] = 0
-for i in range(1, N+1):
-    if i < 3:
-        continue
-    elif 3 <= i < 5:
-        if dp[i-3] >= 0:
-            dp[i] = dp[i-3] + 1
-    else:
-        if dp[i-3] >= 0 and dp[i-5] >= 0:
-            dp[i] = min(dp[i-3] + 1, dp[i-5] + 1)
-        elif dp[i-3] >= 0:
-            dp[i] = dp[i-3] + 1
-        elif dp[i-5] >= 0:
-            dp[i] = dp[i-5] + 1
+dp = [5001] * (n+1)
+dp[3] = 1
+if n >= 5:
+    dp[5] = 1
 
-print(dp[N])
+for idx in range(6, n+1):
+    dp[idx] = min([dp[idx], dp[idx-3] + 1, dp[idx-5] + 1])
+
+if dp[n] == 5001:
+    print(-1)
+else:
+    print(dp[n])
